457c504fdb130400827de5e6833ce80b6aee1bc3
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-21 01:12:27 +0100

    add setup to initiate actors

---
+++ setup.c
@@ -0,0 +1,8 @@
+#include "structs.h"
+
+void setup (Actor *actor) {
+    actor->actorPosition.x = 20.0;
+    actor->actorPosition.y = 20.0;
+    actor->actorVelocity.x = 200.0;
+    actor->actorVelocity.y = 200.0;
+}
---
+++ setup.h
@@ -0,0 +1,8 @@
+#ifndef TEST_PROJECT_SETUP_H
+#define TEST_PROJECT_SETUP_H
+
+#include "structs.h"
+
+void setup (Actor *actor);
+
+#endif //TEST_PROJECT_SETUP_H
