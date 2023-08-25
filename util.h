37dc9a3f84376fdb73ec8fb4229d9b842f89701a
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-25 19:38:59 +0100

    add util file and collision function

---
+++ util.h
@@ -0,0 +1,8 @@
+#ifndef TEST_PROJECT_UTIL_H
+#define TEST_PROJECT_UTIL_H
+
+#include "defs.h"
+
+int collision(int x1, int y1, int h1, int w1, int x2, int y2, int w2, int h2);
+
+#endif //TEST_PROJECT_UTIL_H
