a685f618e4639d514ee5a562b945f67d3aeb0df4
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-26 20:35:00 +0100

    refactor and add utils

--- setup.h
+++ setup.h
@@ -3,6 +3,7 @@
 
 #include "structs.h"
 #include "blit.h"
+#include "util.h"
 #include <SDL_image.h>
 #include <stdlib.h>
 
@@ -11,17 +12,30 @@ static SDL_Texture *enemyTexture;
 static int enemySpawnTimer;
 SDL_Rect screenBounds;
 
-void setup (App *app, Actor *actor, Stage *stage);
+void setup(App *app, Actor *actor, Stage *stage);
+
 static void draw(App *app, Actor *actor, Stage *stage);
+
 static void logic(App *app, Actor *actor, Stage *stage, double deltaTime);
+
 static void setupPlayer(App *app, Actor *player, Stage *stage);
+
 static void handlePlayer(App *app, Actor *actor, Stage *stage, double deltaTime);
+
 static void handleArrows(Stage *stage, double deltaTime);
+
+static int arrowHitEnemy(Stage *stage, Actor *arrow);
+
 static void drawArrows(App *app, Stage *stage);
+
 static void drawPlayer(App *app, Actor *actor);
-static void handleEnemy(Stage *stage,  Actor *player, double deltaTime);
+
+static void handleEnemy(Stage *stage, Actor *player, double deltaTime);
+
 static void spawnEnemy(Stage *stage);
+
 static void drawEnemy(App *app, Stage *stage);
+
 static void fireArrow(Stage *stage, Actor *player);
 
 #endif //TEST_PROJECT_SETUP_H
