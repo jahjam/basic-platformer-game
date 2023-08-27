41282abd0fe0051a8e4e40eec985107240adcac7
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-27 14:32:48 +0100

    refactor

--- animate.c
+++ animate.c
@@ -2,7 +2,9 @@
 
 void animate(Actor *actor, int aniIndex) {
     uint32_t currentFrame =
-            ((SDL_GetTicks() - actor->animationStartTime) * actor->frameRatePerSecond / 1000) % actor->numFrames;
+            ((SDL_GetTicks() - actor->animationStartTime)
+            * actor->frameRatePerSecond / 1000)
+            % actor->numFrames;
 
     actor->srcRectl.y = (int)(aniIndex * actor->srcRectl.h);
     actor->srcRectl.x = (int)(currentFrame * actor->srcRectl.w);
