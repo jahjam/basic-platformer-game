6fbb7021f6471fc07946249ccffe34f55a851975
“jahjam” <elijahjamesuk@hotmail.com>
2023-08-27 14:15:02 +0100

    add animate func

---
+++ animate.c
@@ -0,0 +1,9 @@
+#include "animate.h"
+
+void animate(Actor *actor, int aniIndex) {
+    uint32_t currentFrame =
+            ((SDL_GetTicks() - actor->animationStartTime) * actor->frameRatePerSecond / 1000) % actor->numFrames;
+
+    actor->srcRectl.y = (int)(aniIndex * actor->srcRectl.h);
+    actor->srcRectl.x = (int)(currentFrame * actor->srcRectl.w);
+}
---
+++ animate.h
@@ -0,0 +1,8 @@
+#ifndef TEST_PROJECT_ANIMATE_H
+#define TEST_PROJECT_ANIMATE_H
+
+#include "structs.h"
+
+void animate(Actor *actor, int aniIndex);
+
+#endif //TEST_PROJECT_ANIMATE_H
